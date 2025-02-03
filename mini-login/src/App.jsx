f8752d3ac111f7448f86d7f6cb62fb10881f393b
Jullya Lopes Machado <jullyalopes56@gmail.com>
2025-02-03 14:44:41 -0500

    Added mini-login folder

---
+++ mini-login/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import LoginSingup from './components/LoginSignup'
+
+function App() {
+
+  return (
+    <div>
+      <LoginSingup/>
+    </div>
+  
+  );
+}
+
+export default App
