7b1399dae5a29b7a467700f94b7afaf3cc03b044
Jullya Lopes Machado <jullyalopes56@gmail.com>
2025-02-23 18:14:53 -0500

    Fixed  mini-profile and other untracked files

---
+++ frontend/mini-profile/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserProfile from "./components/UserProfile"; // Import the component
+
+function App() {
+  return (
+    <div>
+      <UserProfile /> {/* Use the component */}
+    </div>
+  );
+}
+
+export default App;
