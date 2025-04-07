64d7d3f087ec9db23faf143958e17c5d57f88bf2
guilherme-felizardo <124207385+guilherme-felizardo@users.noreply.github.com>
2025-04-07 13:04:52 -0400

    bcryptjs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
-const bcrypt = require("bycriptjs");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
